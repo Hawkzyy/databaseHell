a6beae30112651388af6c142b7abaadd6039296f
Hawkzyy <36340874+Hawkzyy@users.noreply.github.com>
2018-11-29 12:00:10 -0500

    Navbar

--- musicDB.php
+++ musicDB.php
@@ -15,18 +15,39 @@
 <head>
   <title>Music Database</title>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="music.css" type="text/css"/>
-  <link rel="stylesheet" href="bootstrap.css" type="text/css"/>
+  <link rel="stylesheet" href="./css/music.css" type="text/css"/>
+  <link rel="stylesheet" href="./css/bootstrap.css" type="text/css"/>
 </head>
 <body>
   <header>
+    <nav class="navbar navbar-default">
+            <div class="container-fluid row">
+                <div class="col-md-5 noshrink">
+                    <a class="navbar-brand pull-left" href="">Music Library</a>
+                </div>
+                <div class="col-md-2">
+                    <form class="pull-left" action="http://www.randyconnolly.com" method="get">
+                            <div class="input-group">
+                                <input class ="form-control" name="search" type="text" placeholder="Search">
+                                <div class= "input-group-btn">
+                                    <button  type="submit" class="btn btn-primary">
+                                        <span class="glyphicon glyphicon-search"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                </div>
+                <div class="col-md-5">
+                </div>
+            </div>
+        </nav>
   </header>
   <main>
     <div class="panel-body">
       <div class="pagewidth container-fluid">
         <div class="row">
           <div class="col-md-12">
-            <h1>Music Library</h1>
+            <!-- <h1>Music Library</h1> -->
             <table border = 1>
             <tr>
               <th>Title</th>
